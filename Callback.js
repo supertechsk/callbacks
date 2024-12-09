c8ecf03209ea382fb3d84296f074324c9b5aaa69
supertechsk <sachinkumar1852sk@gmail.com>
2024-12-09 23:12:18 +0530

    Update Callback.js

--- Callback.js
+++ Callback.js
@@ -1,1 +1,13 @@
 //callback example
+setTimeout(function(){
+    console.log("timer")
+
+},5000);
+function x(y){
+    console.log("x")
+    y();
+
+}
+x(function y(){         // callback function passed in x fun. 
+    console.log("y") 
+});
